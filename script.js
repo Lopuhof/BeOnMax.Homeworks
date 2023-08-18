ec0afa02731037a4f6481e91c3e90df373737644
Evgen <example@gmail.com>
2023-08-18 17:41:38 +0700

    complete lesson 2 about apk

--- script.js
+++ script.js
@@ -38,18 +38,38 @@ function chooseExpenses() {
 
 chooseExpenses();
 
-appData.moneyPerDay = (appData.budget / 30).toFixed(1);
+function detectDayBudget() {
+	appData.moneyPerDay = (appData.budget / 30).toFixed(1);
+	alert('Ежедневный бюджет: ' + appData.moneyPerDay);
+};
 
-alert('Ежедневный бюджет: ' + appData.moneyPerDay);
+detectDayBudget();
 
-if (appData.moneyPerDay < 100) {
-	console.log('Минимальный уровень достатка')
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-	console.log('Средний уровень достатка');
-} else if (appData.moneyPerDay > 2000) {
-	console.log('Высокий уровень достатка');
-} else {
-	console.log('Произошла ошибка!');
+
+function detectLevel() {
+	if (appData.moneyPerDay < 100) {
+		console.log('Минимальный уровень достатка')
+	} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+		console.log('Средний уровень достатка');
+	} else if (appData.moneyPerDay > 2000) {
+		console.log('Высокий уровень достатка');
+	} else {
+		console.log('Произошла ошибка!');
+	};
+};
+
+detectLevel();
+
+function chooseOptExpenses() {
+	for (let i = 0; i < 3; i++) {
+		let answer = prompt('Статья необязательных расходов?: ', '');
+	};
+
+	let optionalExpenses = {
+		1 : answer,
+		2 : answer,
+		3 : answer
+	};
 };
 
 function checkSavings() {
